065669eb5e0ecc4e7cc230a86282c31deb7bc3c0
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-17 22:58:16 +0500

    commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { Logo } from './assets';
+import { logo } from './assets';
 import { Home, CreatePost } from './pages'
-import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
     <BrowserRouter>
     <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+    <Link to='/'>
+      <img src = {logo} alt='logo'
+      className='w-28 object-contain'/>
+    </Link>
 
+    <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+    Create
+    </Link>
     </header>
     </BrowserRouter>
   )
