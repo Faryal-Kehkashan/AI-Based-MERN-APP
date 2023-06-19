37f869eb4a6d51e24d413f26cc4b183b19c0a526
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-19 23:18:26 +0500

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect} from 'react'
 import { Loader, Card, FormField } from '../components'
 
 function Home() {
+
+  const [loading, setLoading] = useState(false);
+  const [allpost, setAllPosts] = useState(null);
+
   return (
     <div>Home</div>
   )
