d7c78ba2d3de227c22d26c0dd34e89e867d30392
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-22 20:31:50 +0500

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,11 @@ const RenderCards = ({ data, title }) => {
   if(data?.length > 0) {
     return data.map((post) => <Card key = {post.id} {...post}/>)
   }
+  return (
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+      {title}
+    </h2>
+  )
   }
 function Home() {
 
