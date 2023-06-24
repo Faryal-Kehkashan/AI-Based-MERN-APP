46c4c284a28c302f9f2c34819f73014dc94b5d51
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-24 15:41:03 +0500

    commit again

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function FormField({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) {
+  return (
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label 
+          htmlFor={name}
+          className='block text-sm font-mediu, text-gray-900'
+        >
+          {LabelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type = "button"
+            onClick = {handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+          >
+            Surprise Me
+          </button>
+        )
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default FormField
