9e15628ad85f91aec25001287ec4d061d00ddfe2
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-25 17:24:45 +0500

    new commit

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -19,9 +19,18 @@ function FormField({ LabelName, type, name, placeholder, value, handleChange, is
             Surprise Me
           </button>
         )
-
         }
       </div>
+      <input 
+        type = {type}
+        id = {name}
+        name = {name}
+        placeholder = {placeholder}
+        value = {value}
+        onChange = {handleChange}
+        required
+        className = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      />
     </div>
   )
 }
