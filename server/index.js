bd01782ce7ebe9696f73495ddc2f1490c73a96f3
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-26 17:29:20 +0500

    not getting pushed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect";
+
 dotenv.config();
 
 const app = express();
@@ -13,9 +15,14 @@ app.get("/", async (req, res) => {
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server has started on port http://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
