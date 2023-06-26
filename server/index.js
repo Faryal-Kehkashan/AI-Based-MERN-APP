50a43840d493b0bbe47d2bb7aa448e4f8ddff0da
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-26 18:05:09 +0500

    new

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
