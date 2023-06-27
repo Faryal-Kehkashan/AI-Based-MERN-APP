2123adbd44b24c3b68b03a59bf3e0cd9434d0d8d
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-27 17:54:39 +0500

    new

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,3 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
-const Post = new mongoose.Schema({});
+const Post = new mongoose.Schema({
+  name: { type: String, required: true },
+  prompt: { type: String, required: true },
+  photo: { type: String, required: true },
+});
