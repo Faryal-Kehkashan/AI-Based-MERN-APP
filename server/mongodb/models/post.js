840163940cd86c64bb1a34c33b962c42bc52f599
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-06-30 00:02:59 +0500

    new

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -4,7 +4,6 @@ const Post = new mongoose.Schema({
   name: { type: String, required: true },
   prompt: { type: String, required: true },
   photo: { type: String, required: true },
-  photo2: { type: String, required: true },
 });
 
 const PostSchema = mongoose.model("Post", Post);
