1dc8877d004e9e2d014c0fe0e476440a37a0c76b
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-07-02 22:40:22 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
-
+import postRoutes from "./routes/postRoutes.js";
+import dallePost from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
