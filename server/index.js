3fe9c125dd6db5733ed5af3328b2c9bae5c94c60
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-07-02 22:43:11 +0500

    new

--- server/index.js
+++ server/index.js
@@ -5,11 +5,15 @@ import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dallePost from "./routes/dalleRoutes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("api/v1/dalle", dallePost);
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E");
