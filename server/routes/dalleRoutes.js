507cd8efc48226e3c1596b2a12eb196c5764a93c
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-07-04 23:07:49 +0500

    new

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -5,3 +5,9 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
