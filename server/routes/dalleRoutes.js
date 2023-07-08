0c96567bd07e5bdeba4d35354e9bd57fe6b7b566
Faryal Kehkashan <96655810+Faryal-Kehkashan@users.noreply.github.com>
2023-07-08 22:20:42 +0500

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,7 +19,21 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-  } catch (error) {}
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
 });
 
 export default router;
