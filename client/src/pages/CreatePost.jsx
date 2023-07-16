24b59f4786509a189d442ccc088efd18f7c25421
Faryal Kehkashan <96655810+Faryal-Kehkashan@users.noreply.github.com>
2023-07-17 00:09:18 +0500

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,6 +22,10 @@ function CreatePost() {
          setGenerateImg(true);
         const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: "POST" ,
+                    headers: {
+            "Content-Type": "application/json",
+          },
+          body: " JSON.stringify({ prompt: form.prompt }),
         })
       } catch (error) {
         
