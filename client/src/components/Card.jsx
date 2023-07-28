1f39ff4da66de91a060eee61c79fcbde696eb557
Faryal Kehkashan <96655810+Faryal-Kehkashan@users.noreply.github.com>
2023-07-28 20:01:00 +0500

    Final Commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,6 +11,30 @@ const Card = ({ _id, name, prompt, photo }) => {
         src={photo}
         alt={prompt}
       />
+      <div className="group-hover:flex fex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-md overflow-y-auto prompt">{prompt}</p>
+
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">
+              {name[0]}
+            </div>
+            <p className="text-white text-sm">{name}</p>
+          </div>
+
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, name)}
+            className="outline-none bg-transparent border-none"
+          >
+            <img
+              src={download}
+              alt="download"
+              className="w-6 h-6 object-contain invert"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
